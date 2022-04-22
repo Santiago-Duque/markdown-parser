43cd856cc630b0f6acfb658ef6086ea3c1f84759
Santiago-Duque <sduque@ucsd.edu>
2022-04-22 13:49:59 -0700

    JUnit Tests To Read Files in MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Files;
+import java.util.*;
+import java.nio.file.Path;
+import java.io.IOException;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
         //testing
+    }
+    @Test
+    public void tryGettingLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(contents));
     }
 }
