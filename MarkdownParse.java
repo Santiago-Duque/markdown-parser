973ca00a6a97867b52c45eb9145b39607a091b37
Santiago-Duque <sduque@ucsd.edu>
2022-05-03 01:05:26 -0700

    Update MarkdownParse.java
    Infinite Loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,12 +12,12 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            //int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", currentIndex); //currentIndex instead of closeBracket
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if (!markdown.substring(currentIndex,markdown.length()).contains("(")) {
+                return toReturn;
+            }
+            else if (!markdown.substring(currentIndex,markdown.length()).contains("[")) {
+                return toReturn;
+            }
         }
 
         return toReturn;
@@ -34,6 +34,13 @@ public class MarkdownParse {
 /*int openBracket = markdown.indexOf("[", currentIndex); //original
 int closeBracket = markdown.indexOf("]", openBracket);
 int openParen = markdown.indexOf("(", closeBracket);
+int closeParen = markdown.indexOf(")", openParen);
+toReturn.add(markdown.substring(openParen + 1, closeParen));
+currentIndex = closeParen + 1;*/
+
+/*int openBracket = markdown.indexOf("[", currentIndex);
+//int closeBracket = markdown.indexOf("]", openBracket); //first code change
+int openParen = markdown.indexOf("(", currentIndex); //currentIndex instead of closeBracket
 int closeParen = markdown.indexOf(")", openParen);
 toReturn.add(markdown.substring(openParen + 1, closeParen));
 currentIndex = closeParen + 1;*/
