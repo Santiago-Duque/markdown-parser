2f832246b61d110ebc7255c28ac38cd1924cb774
Santiago-Duque <sduque@ucsd.edu>
2022-05-03 01:23:29 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,12 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if (!markdown.substring(currentIndex,markdown.length()).contains("(")) {
+                return toReturn;
+            }
+            else if (!markdown.substring(currentIndex,markdown.length()).contains("[")) {
+                return toReturn;
+            }
             if (markdown.contains("[") || markdown.contains("]") && markdown.contains("(") || markdown.contains(")")) {
                 int openBracket = markdown.indexOf("[", currentIndex);
                 int closeBracket = markdown.indexOf("]", openBracket);
@@ -19,12 +25,6 @@ public class MarkdownParse {
                 int closeParen = markdown.indexOf(")", openParen);
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
-            }
-            if (!markdown.substring(currentIndex,markdown.length()).contains("(")) {
-                return toReturn;
-            }
-            else if (!markdown.substring(currentIndex,markdown.length()).contains("[")) {
-                return toReturn;
             }
         }
 
